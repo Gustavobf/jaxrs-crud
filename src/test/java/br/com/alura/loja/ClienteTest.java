1398e64f2f94da50cd7fac557fb3c7b51726b718
Gustavo Batista <gustavobatistafig@hotmail.com>
2021-12-16 13:52:00 -0300

    Tests end-to-end

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,20 +4,42 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
 import junit.framework.Assert;
 
 public class ClienteTest {
 
+	private HttpServer server;
+
+	@Before
+	public void iniciaServidor() {
+		server = Servidor.inicializaServidor();
+	}
+
+	@After
+	public void interrompeServidor() {
+		server.stop();
+	}
+
 	@Test
-	public void testaConexaoComServidor() {
+	public void testaBuscarCarrinho() {
 
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
+		WebTarget target = client.target("http://localhost:8080");
 
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-	    Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+
+		System.out.println(conteudo);
 
 	}
 
